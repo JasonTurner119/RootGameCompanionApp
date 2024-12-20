918b9f73b1bd155ae23d6ca107e11d380a0015b9
JasonTurner <jasonturner119@icloud.com>
2024-12-20 14:38:06 -0800

    Initial Commit

---
+++ Root Companion/Root_CompanionApp.swift
@@ -0,0 +1,17 @@
+//
+//  Root_CompanionApp.swift
+//  Root Companion
+//
+//  Created by Jason Turner on 12/20/24.
+//
+
+import SwiftUI
+
+@main
+struct Root_CompanionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
